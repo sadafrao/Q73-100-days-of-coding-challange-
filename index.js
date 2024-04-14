0d1ec3689f7f61a6a7e155dfed163090bde3e1a3
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-14 21:43:12 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// Q73
+// this function shows how to assign and update variable values
+function update() {
+    var x = 5;
+    console.log(x); // 5
+    x = 10;
+    console.log(x); // 10
+}
+update();
